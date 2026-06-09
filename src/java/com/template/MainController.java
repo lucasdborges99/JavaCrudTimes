a2e2e4b320741688d709e1c4addc9c06509ecce2
Lucas Dalben Borges <ra2457063>
2026-06-09 09:27:24 -0300

    Crud Melhorado

--- src/java/com/template/MainController.java
+++ src/java/com/template/MainController.java
@@ -2,6 +2,7 @@ package com.template;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -14,10 +15,10 @@ import java.util.ArrayList;
 
 public class MainController
 {
-    @FXML private Button btnSalvar;
+    @FXML private Button btnEditar;
     @FXML private Button btnAdicionar;
     @FXML private Button btnExcluir;
-    @FXML private Button btnEditar;
+    @FXML private Button btnPesquisar;
     @FXML private TextField txtId;
     @FXML private TextField txtNome;
     @FXML private TextField txtFund;
@@ -30,6 +31,8 @@ public class MainController
     @FXML private TableColumn<TimesDTO, String> colEstado;
     @FXML private TableColumn<TimesDTO, Integer> colBrasileiros;
 
+    private ObservableList<TimesDTO> ListaParaFiltrar = FXCollections.observableArrayList();
+
     @FXML
     private void initialize()
     {
@@ -40,6 +43,7 @@ public class MainController
         colBrasileiros.setCellValueFactory(new PropertyValueFactory<>("titulosBrasileiros"));
 
         atualizarTabela();
+        gerenciarBotoes(false);
 
         System.out.println("FXML loaded successfully!");
     }
@@ -47,7 +51,9 @@ public class MainController
     private void atualizarTabela() {
         TimesDAO objtimesdao = new TimesDAO();
         ArrayList<TimesDTO> listaTimes = objtimesdao.listarTimes();
-        tblTimes.setItems(FXCollections.observableArrayList(listaTimes));
+
+        ListaParaFiltrar.setAll(listaTimes);
+        tblTimes.setItems(ListaParaFiltrar);
     }
 
     @FXML
@@ -60,6 +66,7 @@ public class MainController
             txtFund.setText(String.valueOf(objtimesdto.getAnoFundacao()));
             txtEstado.setText(objtimesdto.getEstado());
             txtBrasileiros.setText(String.valueOf(objtimesdto.getTitulosBrasileiros()));
+            gerenciarBotoes(true);
         }
     }
 
@@ -80,7 +87,7 @@ public class MainController
         objtimesdao.cadastrarTime(objtimesdto);
 
         atualizarTabela();
-        btnSalvarAction(event);
+        limparCampos();
     }
 
     @FXML
@@ -102,7 +109,7 @@ public class MainController
         objtimesdao.alterarTime(objtimesdto);
 
         atualizarTabela();
-        btnSalvarAction(event);
+        limparCampos();
     }
 
     @FXML
@@ -113,15 +120,41 @@ public class MainController
         objtimesdao.excluirTime(id);
 
         atualizarTabela();
-        btnSalvarAction(event);
+        limparCampos();
     }
 
     @FXML
-    private void btnSalvarAction(ActionEvent event) {
+    private void btnPesquisarAction(ActionEvent event) {
+        FilteredList<TimesDTO> listaFiltrada = new FilteredList<>(ListaParaFiltrar, p -> true);
+
+        listaFiltrada.setPredicate(time -> {
+
+            if (!txtId.getText().isEmpty()) {
+                String filtroId = txtId.getText().trim();
+                if (!String.valueOf(time.getId()).contains(filtroId)) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        tblTimes.setItems(listaFiltrada);
+    }
+
+    private void limparCampos() {
         txtId.clear();
         txtNome.clear();
         txtFund.clear();
         txtEstado.clear();
         txtBrasileiros.clear();
+
+        txtNome.requestFocus();
+        gerenciarBotoes(false);
+    }
+
+    private void gerenciarBotoes(boolean itemSelecionado) {
+        btnAdicionar.setDisable(itemSelecionado);
+        btnEditar.setDisable(!itemSelecionado);
+        btnExcluir.setDisable(!itemSelecionado);
     }
 }
